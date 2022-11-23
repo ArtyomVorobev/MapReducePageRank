c609455097d39e587d41514832d312aa16e26118
Vorobyov Artyom <artyomvorobev>
2022-11-24 02:09:36 +0300

    added page rank algo

--- src/page_rank.py
+++ src/page_rank.py
@@ -1,25 +1,47 @@
-import config as cfg
 from mrjob.job import MRJob
+from mrjob.protocol import JSONProtocol
+from mrjob.step import MRStep
+
 
 class MRPageRank(MRJob):
+
+    INPUT_PROTOCOL = JSONProtocol
+
     def configure_args(self):
         super(MRPageRank, self).configure_args()
         self.add_passthru_arg(
-            '--damping-factor', dest='damping_factor', default=0.85, type='float',
+            '--damping-factor', dest='damping_factor', default=0.85, type=float,
             help='The value of damping factor. Defaults to 0.85.')
 
         self.add_passthru_arg(
-            '--iters', dest='iters', default=20, type='int',
+            '--iters', dest='iters', default=20, type=int,
             help='The number of iterations. Defaults to 20.')
-            
-    def map(self, node_id, node):
-        pass
 
-    def reduce(self, node_id, node_values):
-        pass 
+    def map(self, node_category, node_values):
+        yield node_category, ('node', node_values)
+        if "outer_links" in node_values:
+            for outer_link, link_score in node_values["outer_links"]:
+                yield outer_link, ('pr_score', node_values['pr_score'] * link_score)
+
+    def reduce(self, node_category, node_values):
+        node = {}
+        total_pr = 0
+        prev_score_set = False
+
+        for value_type, value in node_values:
+            if value_type == 'node':
+                node = value
+                if not prev_score_set:
+                    node['previous'] = node['pr_score']
+                    prev_score_set = True
+            elif value_type == 'pr_score':
+                total_pr += value
+
+        node['pr_score'] = 1 - self.options.damping_factor + self.options.damping_factor * total_pr
+        yield node_category, node
 
     def steps(self):
-        return ([self.mr(mapper=self.map, reducer=self.reduce)]*self.options.iters)
+        return [MRStep(mapper=self.map, reducer=self.reduce)]*self.options.iters
 
 
 if __name__ == '__main__':
--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,5 +1,4 @@
 import config as cfg
-import json
 
 from mrjob.protocol import JSONProtocol
 
@@ -11,30 +10,43 @@ class Preprocessor:
         self.json = JSONProtocol()
 
     def prepare_data(self) -> None:
-        categories = {}
         with open(self.data_path, encoding='utf-8') as file:
-            for line in file:
-                links = line.split(' ')
-                node_category = links[0].split('/')[-1]
-                out_category = links[2].split('/')[-1]
-                if node_category not in categories:
-                    categories[node_category] = []
-                categories[node_category].append(out_category)
+            categories = self.extract_categories_from(file)
+
         with open(self.output_path, 'w', encoding='utf-8') as out_file:
-            for category in categories.keys():
-                cur_categories = {}
-                cur_page_rank = 1/len(categories[category])
-                for out_category in categories[category]:
-                    cur_categories[out_category] = (out_category, cur_page_rank)
-            out_file.write(self.write_node(category, cur_categories))
-                
-    def write_node(self, node_category: str, outer_categories: dict, pr_score: float = 1.0) -> str:
-        node = {}
+            self.write_categories_to(out_file, categories)
+
+    @staticmethod
+    def extract_categories_from(file) -> dict:
+        categories = {}
+        for line in file:
+            links = line.split(' ')
+            node_category = links[0].split('/')[-1]
+            out_category = links[2].split('/')[-1]
+            if node_category not in categories:
+                categories[node_category] = []
+            categories[node_category].append(out_category)
+        return categories
+
+    def write_categories_to(self, out_file, categories):
+        nodes_values = {}
+        for category in categories.keys():
+            out_categories = {}
+            cur_page_rank = 1/len(categories[category])
+            for out_category in categories[category]:
+                out_categories[out_category] = cur_page_rank
+            nodes_values[category] = self.get_node_values(out_categories)
+            out_file.write(self.json.write(category, nodes_values[category]).decode() + '\n')
+
+    @staticmethod
+    def get_node_values(outer_categories: dict, pr_score: float = 1.0) -> dict:
+        node_values = {}
         if outer_categories:
-            node['outer_links'] = sorted(outer_categories.items())
-        node['score'] = pr_score
-        return self.json.write(node_category, node) + '\n'
-       
+            node_values['outer_links'] = sorted(outer_categories.items())
+        node_values['pr_score'] = pr_score
+        return node_values
+
 
 if __name__ == '__main__':
-    prepare_data(cfg.DATA_PATH, cfg.OUTPUT_PATH)
+    preprocessor = Preprocessor(cfg.DATA_PATH, cfg.OUTPUT_PATH)
+    preprocessor.prepare_data()
